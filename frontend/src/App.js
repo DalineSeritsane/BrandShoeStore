0a9034136e4f07bf132b445af3897f4917cbfbb3
DalineSeritsane <dalinelerato@gmail.com>
2025-05-04 03:41:23 +0200

    pages and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Category from './Page/Category';
+import Shop from "./Page/Shop";
+import Product from './Page/Product';
+import Cart from "./Page/Cart";
+import Login from "./Page/Login";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
-
-
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/nike' element={<Category category="nike"/>}/>
+        <Route path='/puma' element={<Category category="puma"/>}/>
+        <Route path='/valentino' element={<Category category="valentino"/>}/>
+        <Route path="/product" element={<Product/>}/>
+        <Route path=':productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
  
     
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './navbar.css';
 import logo from "../Assets/logo.jpg";
 import cart from "../Assets/cart.jpg";
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("shop");
   return (
     <div className='navbar'>
         <div className= "nav-logo">
@@ -11,10 +13,10 @@ const Navbar = () => {
             <p>Sneaker FiT</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop</li>
-            <li>Nike</li>
-            <li>Puma</li>
-            <li>Valentino</li>
+            <li onClick= {()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick= {()=>{setMenu("nike")}}>Nike{menu==="nike"?<hr/>:<></>}</li>
+            <li onClick= {()=>{setMenu("puma")}}>Puma{menu==="puma"?<hr/>:<></>}</li>
+            <li onClick= {()=>{setMenu("valentino")}}>Valentino{menu==="valentino"?<hr/>:<></>}</li>
 
         </ul>
         <div className='nav-login-cart'>
